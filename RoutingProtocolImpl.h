7c13b7a57f961f33cd22ff1d09ec5667f7cf35aa
Jacob <chenshyfool@qq.com>
2014-10-20 23:38:18 -0600

    add data structure and LS function to .h

--- RoutingProtocolImpl.h
+++ RoutingProtocolImpl.h
@@ -2,31 +2,55 @@
 #define ROUTINGPROTOCOLIMPL_H
 
 #include "RoutingProtocol.h"
+#include "Simulator.h"
+#include "Node.h"
+#include <map>
+#include <set>
+#include <vector>
+#include <netinet/in.h>
 
   
 class RoutingProtocolImpl : public RoutingProtocol {
-  // routing table
-  struct table {
+
+public:
+
+  // Forwarding table
+  struct forwardTable {
         unsigned short dest_id;
         unsigned short next_hop;
-        int cost;
-  }; 
-  typedef struct table rtable;
+  };
+  typedef struct forwardTable ftable;
   
-  // port table:
+  // port table
   struct port {
         unsigned short port_num;
         unsigned short neighbor_id;
         int cost;
+        bool is_alive;
   };
   typedef struct port rport; 
   
-  public:
+  // DV table
+  struct DVtable { 
+        unsigned short dest_id;
+        unsigned short next_hop;
+        unsigned short port_num;
+        int cost; 
+        int ttl;
+  }; 
+  typedef struct DVtable dvtable;
+  
+  struct _linkState;
+
+  typedef struct _linkState {
+    unsigned int sequence;
+    unsigned short Node_ID;
+	unsigned int update;
+    map<unsigned short, unsigned short> neighbour;
+  } linkState;
+  
   // added code:
-    vector<rtable> routing_table;
-    vector<rport>  port_table;
-    eProtocolType proto_type;
-    unsigned short router_id;  
+  
   
     RoutingProtocolImpl(Node *n);
     ~RoutingProtocolImpl();
@@ -61,11 +85,48 @@ class RoutingProtocolImpl : public RoutingProtocol {
     
     void detect_neighbor(unsigned short router_id, unsigned short port_id);
     void forward_dv();
-    void forward_packet();
+    void forward_packet(); 
+    void merge_route(unsigned short dest, unsigned short next, unsigned short port, int cost);
+    void update_forwarding_table(unsigned short dest, unsigned short next_hop);
+    void show_DV_table();
+    void show_port_table();
+    void show_forwarding_table();
+    void forward_dv( unsigned short src_id, unsigned short dest_id, unsigned short port_id );
 
  private:
-    Node *sys; // To store Node object; used to access GSR9999 interfaces 
+        Node *sys; // To store Node object; used to access GSR9999 interfaces 
+        
+        short num_ports; // The total number of ports on this router
+        
+        eProtocolType proto_type; // 
+        unsigned short router_id;
+    
+        vector<rport>  port_table;
+        vector<dvtable> DV_table;
+        vector<ftable> forwarding_table;
+        unsigned int mySequence;		
+	
+	// map of Link State table
+	map<unsigned short, linkState> nodeVec;
 
+	// update status (tables) for Link State Protocol
+	bool LSUpdate();
+	
+	// Deal with LS packet
+	void recvLS(unsigned short port, void *packet, unsigned short size);
+	
+	// get shortest path by using Dijkstra algorithm
+	void dijkstra();
+	
+	// forward LS packet that is received from others
+	void sendLSRecv(unsigned short port, char *packet, unsigned short size);
+	
+	// flood LS packet to all alive neighbours
+	void sendLSTable();
+
+	//check LS time out
+	void checkLSTimeOut();
+	 
 };
 
 #endif
